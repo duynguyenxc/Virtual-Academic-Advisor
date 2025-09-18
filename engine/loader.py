de5f7d51209584bd2a73686a9bc98adf2aeba3b4
Nguyen Duc Duy <duydeptrai1611@gmail.com>
2025-09-17 23:28:35 -0500

    add comment

--- engine/loader.py
+++ engine/loader.py
@@ -1,1 +1,5 @@
-
+# engine/loader.py
+# ------------------------------------------------------------
+# Loader for JSON data (courses, degree requirements, plans, policies).
+# Converts raw JSON into Python objects defined in engine/model.py.
+# ------------------------------------------------------------
